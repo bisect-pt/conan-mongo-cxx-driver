1c1a38c4255c56ea0d1f0a24a16af3dd65cd8435
Philip <philip.j.m@gmail.com>
2018-08-29 12:03:04 +0100

    Expand test_package checks
    Adds example code taken from http://mongodb.github.io/mongo-cxx-driver/mongocxx-v3/installation/. This is useful for ensuring the ABI is set correctly.

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -6,6 +6,7 @@
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
 #include <bsoncxx/json.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
 
 using namespace bsoncxx;
 using namespace mongocxx;
@@ -13,7 +14,23 @@ using namespace mongocxx;
 int main()
 {
     const mongocxx::instance instance_; // This should be done only once.
-    auto c = mongocxx::client{mongocxx::uri{}};
+    auto conn = mongocxx::client{mongocxx::uri{}};
+
+    bsoncxx::builder::stream::document document{};
+
+    if (false) // compile this don't run it
+    {
+		auto collection = conn["testdb"]["testcollection"];
+		document << "hello" << "world";
+
+		collection.insert_one(document.view());
+		auto cursor = collection.find({});
+
+		for (auto&& doc : cursor)
+		{
+			std::cout << bsoncxx::to_json(doc) << std::endl;
+		}
+	}
 
     std::cout << "It works!\n";
     return EXIT_SUCCESS;
