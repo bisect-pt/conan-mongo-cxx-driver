929af2dd7767f3849eb448d51dbb6c8967139751
Philip <philip.j.m@gmail.com>
2018-08-29 13:38:59 +0100

    Add build for use_17_standard = false

--- build.py
+++ build.py
@@ -6,4 +6,5 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"], args="--build missing")
     builder.add_common_builds()
+    builder.add({}, {"use_17_standard" : False}, {}, {})
     builder.run()
