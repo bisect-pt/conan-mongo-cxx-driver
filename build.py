622098cbf925d973da8d4f82969ebabaf6cbcb03
Philip <philip.j.m@gmail.com>
2018-08-30 06:51:11 +0100

    Fix settings in build.py

--- build.py
+++ build.py
@@ -6,5 +6,5 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"], args="--build missing")
     builder.add_common_builds()
-    builder.add({}, {"use_17_standard" : False}, {}, {})
+    builder.add({}, {"mongo-cxx-driver:use_17_standard" : False}, {}, {})
     builder.run()
